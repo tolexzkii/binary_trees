e87c64bca3ac86030a5b798704cc684b54385475
Ade-ojo Tolulope <tolucreatart@gmail.com>
2023-03-01 11:12:33 +0100

    binary trees

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	return ((!node->left && !node->right) ? 1 : 0);
+}
+
+/**
+ * binary_tree_leaves- function that counts the
+ * number of leaves in a binary tree
+ * @tree: ptr to the root node of the tree to count the leaves in
+ * Return: number of leaves in binary tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_leaf(tree))
+		return (1);
+
+
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+	return (left + right);
+}
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root of the tree to measure
+ * Return: height or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t depth_left, depth_right, height;
+
+	if (tree == NULL)
+		return (0);
+
+	depth_left = binary_tree_height(tree->left);
+	depth_right = binary_tree_height(tree->right);
+	height = ((depth_left > depth_right) ? depth_left : depth_right) = 1;
+
+	return(height);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: balance factor or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	return (left - right);
+}
